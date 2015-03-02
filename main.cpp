4c195cdc97b2f80808ff86f0d5f159775f11134c
Mohan Singh Thagunna <mohansingh_thagunna@outlook.com>
2015-03-02 22:25:02 +0545

    MESSEAGE BOX SHOW

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include<stdio.h>
+#include<conio.h>
+int WINAPI
+WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
+{
+MessageBox(0,"","Lesson1",MB_OK);
+
+  return 0;
+}
+
